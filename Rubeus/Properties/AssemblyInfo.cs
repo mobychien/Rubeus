e602b84f47012f5a4eec8c442de452ff94b9f78d
YP Chien <ypchien@cox.net>
2020-06-13 10:20:13 -0700

    Update Description

--- Rubeus/Properties/AssemblyInfo.cs
+++ Rubeus/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TDNite")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(" AD HealthCheck")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DOJ")]
 [assembly: AssemblyProduct("TDNite")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
